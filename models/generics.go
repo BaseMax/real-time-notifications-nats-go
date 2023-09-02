1145d0b64ce7672609d39006ecc71acb014cb025
alireza <alirezaarzehgar82@gmail.com>
2023-09-02 19:13:55 +0330

    models: add getting query functions
    - Handle some errors on a generic function
    - Implement `FindById()`
    - Implement `GetAll()`
    - Implement `DeleteById()`
    - Add omitempty to `User{}` model tags
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- models/generics.go
+++ models/generics.go
@@ -27,12 +27,31 @@ func Create[T any](model *T) *DbErr {
 	return nil
 }
 
+func errGetGormToHttp(err error) *DbErr {
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return &DbErr{HttpErr: *echo.ErrNotFound, Err: err}
+	} else if err != nil {
+		return &DbErr{HttpErr: *echo.ErrInternalServerError, Err: err}
+	}
+	return nil
+}
+
 func FindById[T any](id uint) (model *T, e *DbErr) {
 	err := db.First(&model, id).Error
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return nil, &DbErr{HttpErr: *echo.ErrNotFound, Err: err}
-	} else if err != nil {
-		return nil, &DbErr{HttpErr: *echo.ErrInternalServerError, Err: err}
+	return model, errGetGormToHttp(err)
+}
+
+func GetAll[T any](sel string) (models *[]T, e *DbErr) {
+	var err error
+	if sel == "" {
+		err = db.Find(&models).Error
+	} else {
+		err = db.Select(sel).Find(&models).Error
 	}
-	return
+	return models, errGetGormToHttp(err)
+}
+
+func DeleteById(id uint, model any) (e *DbErr) {
+	r := db.Delete(model, id)
+	return errGetGormToHttp(r.Error)
 }
--- models/users.go
+++ models/users.go
@@ -10,7 +10,7 @@ import (
 type User struct {
 	ID       uint   `gorm:"primaryKey" json:"id"`
 	Username string `gorm:"not null; unique" json:"username"`
-	Password string `gorm:"not null" json:"password"`
+	Password string `gorm:"not null" json:"password,omitempty"`
 }
 
 func Login(u *User) *DbErr {
