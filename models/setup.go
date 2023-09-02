6206a15b8c1659abc6d054bc1e3112ab5697b530
alireza <alirezaarzehgar82@gmail.com>
2023-09-02 16:23:35 +0330

    models: create default admin
    - Replace HashPassword to setup
    - Implement `GetAdminConf()`
    - Create default admin on Init
    - Implement `Login()`
    
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- models/setup.go
+++ models/setup.go
@@ -1,10 +1,38 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"os"
+
+	"gorm.io/gorm"
+)
 
 var db *gorm.DB
 
+func HashPassword(pass string) string {
+	hashByte := sha256.Sum256([]byte(pass))
+	hashStr := hex.EncodeToString(hashByte[:])
+	return hashStr
+}
+
+func GetAdminConf() *User {
+	return &User{Username: os.Getenv("ADMIN_USERNAME"), Password: os.Getenv("ADMIN_PASSWORD")}
+}
+
 func Init(externalDb *gorm.DB) error {
 	db = externalDb
-	return db.AutoMigrate(&User{}, &Activity{}, &Order{}, &Product{}, &Refund{})
+	if err := db.AutoMigrate(&User{}, &Activity{}, &Order{}, &Product{}, &Refund{}); err != nil {
+		return err
+	}
+
+	admin := GetAdminConf()
+	admin.Password = HashPassword(admin.Password)
+
+	var count int64
+	db.First(&admin).Count(&count)
+	if count == 1 {
+		return nil
+	}
+	return db.Create(admin).Error
 }
--- models/users.go
+++ models/users.go
@@ -1,7 +1,24 @@
 package models
 
+import (
+	"errors"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
 type User struct {
 	ID       uint   `gorm:"primaryKey" json:"id"`
 	Username string `gorm:"not null; unique" json:"username"`
 	Password string `gorm:"not null" json:"password"`
+}
+
+func Login(u *User) *DbErr {
+	err := db.Where(&u).First(&u).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return &DbErr{Err: err, HttpErr: *echo.ErrNotFound}
+	} else if err != nil {
+		return &DbErr{Err: err, HttpErr: *echo.ErrInternalServerError}
+	}
+	return nil
 }
