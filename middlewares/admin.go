b063c5816109bb06b0a5b23d64100bef52f79e36
alireza <alirezaarzehgar82@gmail.com>
2023-09-02 21:25:41 +0330

    middlewares: using `helpers.IsUserAdmin()` on `IsAdmin()`
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- middlewares/admin.go
+++ middlewares/admin.go
@@ -4,14 +4,11 @@ import (
 	"github.com/labstack/echo/v4"
 
 	"github.com/BaseMax/real-time-notifications-nats-go/helpers"
-	"github.com/BaseMax/real-time-notifications-nats-go/models"
 )
 
 func IsAdmin(next echo.HandlerFunc) echo.HandlerFunc {
 	return echo.HandlerFunc(func(c echo.Context) error {
-		_, loggeninName := helpers.GetLoggedinInfo(c)
-		user := models.GetAdminConf()
-		if loggeninName != user.Username {
+		if !helpers.IsUserAdmin(c) {
 			return echo.ErrUnauthorized
 		}
 		return next(c)
