9616d5dc020fa92ee0d57f0edbb7e329c05aa55d
alireza <alirezaarzehgar82@gmail.com>
2023-09-03 19:20:23 +0330

    maint,controllers: add order prototypes
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- controllers/orders.go
+++ controllers/orders.go
@@ -1,1 +1,43 @@
 package controllers
+
+import "github.com/labstack/echo/v4"
+
+func AddOrder(c echo.Context) error {
+	return nil
+}
+
+func FetchOrder(c echo.Context) error {
+	return nil
+}
+
+func FetchAllOrders(c echo.Context) error {
+	return nil
+}
+
+func EditOrder(c echo.Context) error {
+	return nil
+}
+
+func DeleteOrder(c echo.Context) error {
+	return nil
+}
+
+func FetchOrderStatus(c echo.Context) error {
+	return nil
+}
+
+func CancelOrder(c echo.Context) error {
+	return nil
+}
+
+func GetFirstQueuedOrder(c echo.Context) error {
+	return nil
+}
+
+func DoneFirstQueuedOrder(c echo.Context) error {
+	return nil
+}
+
+func CancelFirstQueuedOrder(c echo.Context) error {
+	return nil
+}
