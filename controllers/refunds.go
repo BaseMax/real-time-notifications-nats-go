19fe486f5eafd1c5ba2d6a84c04840f187dc8c58
alireza <alirezaarzehgar82@gmail.com>
2023-09-05 16:05:27 +0330

    maint,controllers: add refund prototypes
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- controllers/refunds.go
+++ controllers/refunds.go
@@ -1,1 +1,43 @@
 package controllers
+
+import "github.com/labstack/echo/v4"
+
+func AddRefund(c echo.Context) error {
+	return nil
+}
+
+func FetchRefund(c echo.Context) error {
+	return nil
+}
+
+func FetchAllRefunds(c echo.Context) error {
+	return nil
+}
+
+func EditRefund(c echo.Context) error {
+	return nil
+}
+
+func DeleteRefund(c echo.Context) error {
+	return nil
+}
+
+func FetchRefundStatus(c echo.Context) error {
+	return nil
+}
+
+func CancelRefund(c echo.Context) error {
+	return nil
+}
+
+func GetFirstQueuedRefund(c echo.Context) error {
+	return nil
+}
+
+func DoneFirstQueuedRefund(c echo.Context) error {
+	return nil
+}
+
+func CancelFirstQueuedRefund(c echo.Context) error {
+	return nil
+}
